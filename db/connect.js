c0987f5a283fab1b88e259dca58cb85ff827e08b
Chase Pope <156484699+chasepope47@users.noreply.github.com>
2026-05-27 11:28:01 -0600

    Change database name from 'contacts' to 'pantry'

--- db/connect.js
+++ db/connect.js
@@ -1,18 +1,14 @@
 const { MongoClient } = require('mongodb');
-
 let db;
 
 const connectToDb = (callback) => {
   MongoClient.connect(process.env.MONGODB_URI)
     .then((client) => {
-      db = client.db('contacts'); // your database name
+      db = client.db('pantry');
       callback();
     })
-    .catch((err) => {
-      console.error(err);
-    });
+    .catch((err) => console.error(err));
 };
 
 const getDb = () => db;
-
 module.exports = { connectToDb, getDb };
